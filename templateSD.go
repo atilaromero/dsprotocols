4c1e3d0ede2023d9ff436a739f234004ebb5d1bd
Tarcisio Ceolin Junior <tarcisio@ceolin.org>
2018-08-11 12:10:03 -0300

    Added template

---
+++ templateSD.go
@@ -0,0 +1,76 @@
+// -----------------------------------------
+// Tentativa de um Template para o projeto de SD.
+//
+// Disciplina Topicos PPGCC - PUCRS - Escola Politecnica
+// Atila, Dotti, Eliã, Tarcisio (em ordem alfabetica)
+// -----------------------------------------
+
+package main
+
+import (
+	"fmt"
+	// "sync"
+	//"time"
+)
+
+// -----------------------------------------
+// Definição de Template -------------------
+// -----------------------------------------
+// Template aqui nao significa uma estrutura herdada, pois acho que Go nao tem heranca
+// Mas sim um formato que seguiriamos convencionando a estrutura de todos os modulos
+// para suportar as definicoes do livro que seguiremos
+// este formato teria que ter mapeamento o mais direto possivel com as construcoes 
+// do livro
+
+type Template struct {       
+	req, ind chan struct{} // canais oferecidos na api
+	uT1: Template          // assim como declaramos os modulos utilizados por este modulo
+	                       // os canais oferecidos por este estao na sua definicao
+	state: int			   // teriamos ainda aqui tudo referente ao estado deste modulo
+}
+
+func NewTemplate( x Template // used template deveria vir com referencia aos modulos que vai utilizar	
+	             ) *Template {
+					 
+	my := &Template{           // aqui inicializamos canais, referenciamos modulos utilizados, inicializamos estado
+		req: make(chan struct{}),
+		ind: make(chan struct{}),
+		uT1: x                 // assim como declaramos os modulos utilizados por este modulo
+		                       // os canais oferecidos por este estao na sua definicao
+		state: int			   // teriamos ainda aqui tudo referente ao estado deste modulo
+	}
+
+    TemplateEvents () {    // aqui escrevemos o tratamento de eventos - funcao dentro de NewTemplate
+	   select {
+		   case <-my.req:
+			 
+		   case <-uT1.ind:  // aqui pode mandar msg para my.ind:     my.ind <- msgind
+			             // e/ou mandar msg para uT1.req:           uT1.req <- msgreq
+						 // e/ou mudar estado interno.
+						 // noi caso de mudar estado interno teria que ver 
+						 // se guardas internas seriam automaticamente reavaliadas pelas construcoes 
+						 // da linguagem
+	       // case - enfim, aqui teriamos que ver se modelamos os casos necessarios de guardas					
+		}
+	}
+
+
+	go func() { for { templateEvents() }  } // codigo fixo indicando que fica para sempre tratando eventos	
+		
+	}()
+	return my
+}
+
+
+// -----------------------------------------
+// Fim Definição de Template ---------------
+// -----------------------------------------
+
+// Usa template 
+
+
+func main() {
+	fmt.Println("inicio")
+	perfectLink := NewTemplate()
+	perfectLink.req <- mensagemNoLink
+}
