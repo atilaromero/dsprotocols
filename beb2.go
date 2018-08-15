e5d9a70cb2daf9b190e6c30de192104f40a7529b
Atila Romero <atilaromero@gmail.com>
2018-08-15 17:01:42 -0300

    beb experiments

---
+++ beb2.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"sync"
+)
+
+type process struct {
+	id int
+	c  chan []byte
+}
+
+func newProcess(id int) process {
+	return process{
+		id,
+		make(chan []byte),
+	}
+}
+
+type beb2 struct {
+	channels []chan []byte
+}
+
+func (b *beb2) send(msg []byte) {
+	wg := new(sync.WaitGroup)
+	for _, c := range b.channels {
+		wg.Add(1)
+		go func(c chan []byte) {
+			c <- msg
+			defer wg.Done()
+		}(c)
+	}
+	wg.Wait()
+}
