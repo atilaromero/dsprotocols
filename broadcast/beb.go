1763fcfa8b6980d05cc5c8b6fa54ba05468b0ba7
Atila Romero <atilaromero@gmail.com>
2018-08-31 00:46:16 -0300

    comments

--- broadcast/beb.go
+++ broadcast/beb.go
@@ -26,13 +26,27 @@ type Beb struct {
 	Ind     chan BebDelivertMsg
 }
 
+// NewBeb returns a Beb struct, which implements Best Effort Broadcast.
+//
+// There are 4 channels here: the 2 channels from the perfect link
+// are used for inter process communication, while req and ind are used to
+// start and finish the broadcast.
+//
+// New broadcasts are initiated sending a message to the req channel.
+// When an ongoing broadcast is received from another process,
+// a deliver is triggered through the ind channel.
+//
+// Beb.Ind should be read from an upper layer, otherwise the ind
+// channel will stay blocked on the first message.
 func NewBeb(pl link.Pl, numproc int) Beb {
 	req := make(chan BebBroadcastMsg)
 	ind := make(chan BebDelivertMsg)
 	beb := Beb{numproc, pl, req, ind}
 
 	go func() {
+		// on new broadcast request
 		for msg, ok := <-beb.Req; ok; msg, ok = <-beb.Req {
+			// send the message to all known processes through each one's perfect link
 			for q := 0; q < numproc; q++ {
 				pl.Req <- link.PlSendMsg{
 					Dst:     q,
@@ -43,7 +57,9 @@ func NewBeb(pl link.Pl, numproc int) Beb {
 	}()
 
 	go func() {
+		// when receiving a broadcast from another process
 		for msg, ok := <-pl.Ind; ok; msg, ok = <-pl.Ind {
+			// deliver the message one layer up
 			beb.Ind <- BebDelivertMsg{
 				ID:      pl.ID,
 				Payload: msg.Payload,
